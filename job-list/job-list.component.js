af282bb0498a5f37d00245d24aad0a2516571a2f
kannadhaasan <kannadhaasan@outlook.com>
2018-12-02 20:30:16 +0000

    changes in list components

--- job-list/job-list.component.js
+++ job-list/job-list.component.js
@@ -1,1 +1,33 @@
 'use strict';
+// Register `jobList` component, along with its associated controller and template angular.
+angular.module('jobList').
+  component('jobList', {
+    templateUrl: 'job-list/job-list.template.html',
+    controller: function JobListController($scope, JobsService) {
+      debugger;
+      
+      if ($scope.query !== undefined) {
+        JobsService.getJobs().then(function (response) {
+          $scope.jobs = response.data;
+        });
+      }
+
+      $scope.getJobs = function () {
+        debugger;
+        if ($scope.query !== undefined) {
+          JobsService.getJobs($scope.query).then(function (response) {
+            debugger;
+            $scope.jobs = response.data;
+            $scope.isResult = true;
+          }).catch(function (error) {
+            $scope.NotFoundMessage=true;
+            console.log(error);
+            return;
+          });
+        }
+        $scope.NotFoundMessage=false;
+        $scope.isResult = false;
+        $scope.orderProp = 'job_title';
+      }
+    }
+  });
