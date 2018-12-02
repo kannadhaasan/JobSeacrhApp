5aa007757f5559c47df97783ee3419759a60a1f8
kannadhaasan <kannadhaasan@outlook.com>
2018-12-02 20:53:51 +0000

    service to connect to api

---
+++ app/core/job/jobs.service.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.
+  module('core.job', []).
+  factory('JobsServiceFactory', ['$http',
+    function ($http) {
+      var factory = {};
+
+      factory.getJobs = function (query) {
+        return $http.get('http://api.dataatwork.org/v1/jobs/autocomplete?contains=' + query);
+      }
+      factory.getJobsByuuid = function (uuid) {
+        return $http.get('http://api.dataatwork.org/v1/jobs/' + uuid);
+      }
+      factory.getRelatedSkills = function (uuid) {
+        return $http.get('http://api.dataatwork.org/v1/jobs/' + uuid + "/related_skills");
+      }
+      factory.getRelatedJobs = function (uuid) {
+        return $http.get('http://api.dataatwork.org/v1/jobs/' + uuid + "/related_jobs");
+      }
+      return factory;
+    }
+  ]).
+  service('JobsService', function (JobsServiceFactory) {
+    this.getJobs = function (query) {
+      return JobsServiceFactory.getJobs(query);
+    }
+    this.getJobsByuuid = function (uuid) {
+      return JobsServiceFactory.getJobsByuuid(uuid);
+    }
+    this.getRelatedSkills = function (uuid) {
+      return JobsServiceFactory.getRelatedSkills(uuid);
+    }
+    this.getRelatedJobs = function (uuid) {
+      return JobsServiceFactory.getRelatedJobs(uuid);
+    }
+  });
+
